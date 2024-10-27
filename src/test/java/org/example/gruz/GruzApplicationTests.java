5a3332d1faed4448670fe1ed640fa1d22532e8ca
Kosta <prc.zrp@mail.ru>
2024-10-27 16:15:28 +0300

    "sfdcd"

---
+++ src/test/java/org/example/gruz/GruzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
